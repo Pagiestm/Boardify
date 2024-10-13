f51aeb6ba8e52e10f211637cdcb83363abba483f
pagiestm <pagiestm@gmail.com>
2024-10-13 16:37:33 +0200

    Project setup + adding components from shadcn + customizing

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export default function Home() {
+  return (
+    <div>
+      <Input />
+      <Button>Click me</Button>
+      <p className="text-red-600">Théotime</p>
+    </div>
+  )
+}
