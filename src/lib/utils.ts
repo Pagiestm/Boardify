9693fb8f2c5e7df5f4c9a8b076711959d2b0128f
pagiestm <pagiestm@gmail.com>
2024-10-23 23:29:39 +0200

    feat(config, members, utils): :sparkles: Creating the members table + creating workspace members + separating workspaces based on the authenticated user
    Creation of the members table with the following fields: workspaceId, role, userId. When creating a new workspace, the user is the admin and is assigned this role in the workspace table and generation of an invitation code for future members

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generatreInviteCode(lenght: number) {
+  const characters = "ABCDEFGHIJKLMOQPRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let result = ""
+  for (let i = 0; i < lenght; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return result
+}
