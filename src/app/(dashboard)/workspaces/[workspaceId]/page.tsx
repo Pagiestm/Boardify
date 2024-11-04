871b197d1beb86258b1bcd61e32ac96ec14ca5c3
pagiestm <pagiestm@gmail.com>
2024-11-04 22:27:57 +0100

    feat(standalone, layout.tsx): :sparkles: Building a standalone layout

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,7 +1,14 @@
-const WorkspaceIdPage = () => {
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/actions";
+
+const WorkspaceIdPage = async () => {
+    const user = await getCurrent()
+    if (!user) redirect("/sign-in")
+
     return (
         <div>
-            <h1>WorkspaceIdPage</h1>
+            Workspace Id
         </div>
     );
 }
