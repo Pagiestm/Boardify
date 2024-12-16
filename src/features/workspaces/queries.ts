8eb78e0b4126b0999b6ec40dc7bf047d18da9d18
pagiestm <pagiestm@gmail.com>
2024-12-16 23:17:28 +0100

    feat(projects, loading, error): :sparkles: Implement project types, loading, and error pages across various components

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -8,35 +8,32 @@ import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config"
 import { Workspace } from "./types"
 
 export const getWorkspaces = async () => {
-    try {
-        const { databases, account } = await createSessionClient()
-        const user = await account.get()
+    const { databases, account } = await createSessionClient()
 
-        const members = await databases.listDocuments(
-            DATABASE_ID,
-            MEMBERS_ID,
-            [Query.equal("userId", user.$id)]
-        )
+    const user = await account.get()
 
-        if (members.total === 0) {
-            return { documents: [], total: 0}
-        }
+    const members = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        [Query.equal("userId", user.$id)]
+    )
 
-        const workspaceIds = members.documents.map((member) => member.workspaceId)
-
-        const workspaces = await databases.listDocuments(
-            DATABASE_ID,
-            WORKSPACES_ID,
-            [
-                Query.orderDesc("$createdAt"),
-                Query.contains("$id", workspaceIds),
-            ]
-        )
-
-        return workspaces
-    } catch {
-        return { documents: [], total: 0}
+    if (members.total === 0) {
+        return { documents: [], total: 0 }
     }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId)
+
+    const workspaces = await databases.listDocuments(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        [
+            Query.orderDesc("$createdAt"),
+            Query.contains("$id", workspaceIds),
+        ]
+    )
+
+    return workspaces
 }
 
 interface GetWorkspaceProps {
@@ -44,30 +41,27 @@ interface GetWorkspaceProps {
 }
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
-    try {
-        const { databases, account } = await createSessionClient()
-        const user = await account.get()
+    const { databases, account } = await createSessionClient()
 
-        const member = await getMember({
-            databases,
-            workspaceId,
-            userId: user.$id,
-        })
+    const user = await account.get()
 
-        if (!member) {
-            return null
-        }
+    const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+    })
 
-        const workspace = await databases.getDocument<Workspace>(
-            DATABASE_ID,
-            WORKSPACES_ID,
-            workspaceId,
-        )
-
-        return workspace
-    } catch {
-        return null
+    if (!member) {
+        throw new Error("Unauthorized")
     }
+
+    const workspace = await databases.getDocument<Workspace>(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId,
+    )
+
+    return workspace
 }
 
 interface GetWorkspaceInfoProps {
@@ -75,19 +69,15 @@ interface GetWorkspaceInfoProps {
 }
 
 export const getWorkspaceInfo = async ({ workspaceId }: GetWorkspaceInfoProps) => {
-    try {
-        const { databases } = await createSessionClient()
+    const { databases } = await createSessionClient()
 
-        const workspace = await databases.getDocument<Workspace>(
-            DATABASE_ID,
-            WORKSPACES_ID,
-            workspaceId,
-        )
+    const workspace = await databases.getDocument<Workspace>(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId,
+    )
 
-        return {
-            name: workspace.name,
-        }
-    } catch {
-        return null
+    return {
+        name: workspace.name,
     }
 }
