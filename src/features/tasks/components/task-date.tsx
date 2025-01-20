892a13683466351c888820a6ea2418ceb155a2c9
pagiestm <pagiestm@gmail.com>
2025-01-20 23:02:45 +0100

    fix(tasks): :bug: Correct date formatting in TaskDate component to use endDate instead of value

--- src/features/tasks/components/task-date.tsx
+++ src/features/tasks/components/task-date.tsx
@@ -24,7 +24,7 @@ export const TaskDate = ({ value, className }: TaskDateProps) => {
     return (
         <div className={textColor}>
             <span className={cn("truncate", className)}>
-                {format(value, "PPP")}
+                {format(endDate, "PPP")}
             </span>
         </div>
     )
